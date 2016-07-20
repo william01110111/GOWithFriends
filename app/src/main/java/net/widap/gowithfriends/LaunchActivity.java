4a3550dc8727bf779a10e4e7b055663fef9ece7f
William W Wold <wilcoe@rocketmail.com>
2016-07-19 22:13:12 -0700

    added a bunch of files that appear to have not been added automatically

---
+++ app/src/main/java/net/widap/gowithfriends/LaunchActivity.java
@@ -0,0 +1,17 @@
+package net.widap.gowithfriends;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LaunchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_launch);
+        Intent service = new Intent(this, MainService.class);
+        startService(service);
+
+    }
+}
